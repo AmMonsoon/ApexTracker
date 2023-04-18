80c76f0124776dc20d36775a9f2f3c0893e6853d
Alex Monson <monson.alex@rocketmail.com>
2023-04-17 23:22:02 -0700

    working on getting api key

--- apex-tracker/src/PlayerList.js
+++ apex-tracker/src/PlayerList.js
@@ -1,5 +1,25 @@
-function PlayerList() {
+import { useEffect, useState } from  'react'
+import axios from 'axios'
 
+
+const PlayerList = () => {
+   const [players, setPlayers] = useState([])
+
+ 
+    useEffect(() => {
+      getPlayers()
+    },[])
+
+  
+
+  const getPlayers = () => {
+    axios.get(`https://api.mozambiquehe.re/leaderboard?auth=YOUR_API_KEY&legend=LEGEND&key=KEY&platform=PLATFORM`)
+         .then((res) => {
+          setPlayers(res)
+          console.log(players)
+         })
+         .catch((err => console.error(err)))
+  }
 
     return (
       <div className="player-list">
